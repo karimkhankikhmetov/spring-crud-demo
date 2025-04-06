24c48204b334f797a85e8a69716329aa4fe5c6bb
Karimkhan Kikhmetov <kingsleycomangoat@gmail.com>
2025-04-07 02:49:12 +0500

    Initial commit

---
+++ src/test/java/com/project/todotasks/TodotasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.todotasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
